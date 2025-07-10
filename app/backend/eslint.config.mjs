79ccb1f771e0cd0c8b0ac754e0473aca6ddf6fec
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-07-11 08:50:04 +0900

    update: test config

--- app/backend/eslint.config.mjs
+++ app/backend/eslint.config.mjs
@@ -14,4 +14,5 @@ export default defineConfig([
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {languageOptions: { globals: globals.node }},
 ]);
--- app/backend/jest.config.js
+++ app/backend/jest.config.js
@@ -1,11 +1,24 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
+  roots: ["<rootDir>/test", "<rootDir>/src"],
   transform: {
-    ...tsJestTransformCfg,
+    // @swc/jestを使用してテストを高速に実行する
+    '^.+\\.tsx?$': ['@swc/jest', {}],
   },
+   moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/src/index.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!**/node_modules/**",
+    "!**/index.ts",
+  ],
+  // coverageDirectory: 'coverage_dir',
 };
